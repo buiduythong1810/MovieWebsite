a69af8c3b4fcdee9ee643a92f5b22fb473ce00b2
QuocNguyen254 <nguyenanhq254@gmail.com>
2024-05-13 23:08:33 +0700

    Fith commit

---
+++ server/dbConnect.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+    const connectionParams = { useNewUrlParser: true };
+    mongoose.connect("mongodb://localhost:27017/Movie_Web", connectionParams);
+
+    mongoose.connection.on("connected", () => {
+		console.log("Connected to database sucessfully");
+	});
+
+	mongoose.connection.on("error", (err) => {
+		console.log("Error while connecting to database :" + err);
+	});
+
+	mongoose.connection.on("disconnected", () => {
+		console.log("Mongodb connection disconnected");
+	});
+};
+
+// const dbConnect = async () => {
+//     try {
+//         await mongoose.connect("mongodb://localhost:27017");
+//         console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+//     } catch (error) {
+//         console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+//     }
+// };
+
+module.exports = dbConnect;
